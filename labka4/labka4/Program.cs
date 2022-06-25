a077726c39db8389aa0b28367bce31a71767652c
Oleninkaa <olyarosnovska@gmail.com>
2022-06-25 14:55:40 +0200

    lab4

---
+++ labka4/labka4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Cinema florencia = new Cinema("23.08.2001", 125, 4);
+            Console.WriteLine(florencia);
+            Console.WriteLine("Дата побудови: " + florencia.Date);
+            Console.WriteLine("Вартiсть квитка (пiльговий): " + florencia.CountPrice(50, 3));
+            Console.WriteLine("Вартiсть квитка (звичайний): " + florencia.CountPrice(50, 30));
+            Console.WriteLine("Мiсткiсть: " + florencia.capacity());
+
+            Hotel California = new Hotel("10.02.1987", 500, 8);
+            Console.WriteLine("\n" + California);
+            Console.WriteLine("Дата побудови: " + California.Date);
+            Console.WriteLine("Вартiсть оренди: " + California.CountRent(125, 4, 12));
+            Console.WriteLine("Мiсткiсть: " + California.capacity());
+
+            PublicBuilding museum = new PublicBuilding("05.12.1962", 180, 3);
+            Console.WriteLine("\n" + museum);
+            Console.WriteLine("Дата побудови: " + museum.Date);
+            Console.WriteLine("Вартiсть квитка (звичайний): " + museum.CountPrice(100, 34));
+            Console.WriteLine("Вартiсть квитка (пiльговий): " + museum.CountPrice(100, 84));
+            Console.WriteLine("Мiсткiсть: " + museum.capacity());
+
+            ResidentialBuilding house = new ResidentialBuilding("26.01.2015", 175, 19);
+            Console.WriteLine("\n" + house);
+            Console.WriteLine("Дата побудови: " + house.Date);
+            Console.WriteLine("Вартiсть оренди: " + house.CountRent(175, 45, 28));
+            Console.WriteLine("Мiсткiсть: " + house.capacity());
+
+            List<Building> building_list = new List<Building> { florencia, California, museum };
+            City misto = new City(building_list);
+            misto.show_building();
+            misto.add_building(house);
+            misto.show_building();
+            misto.average_capacity();
+        }
+    }
+}
---
+++ labka4/labka4/buildings.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public interface IRent
+{
+	public int CountRent(int price, int quantity, int days);
+}
+
+public interface ITicket
+{
+	public int CountPrice(int price, int customer_age);
+}
+
+public abstract class Building
+{
+	public string Date { get; set; }
+	public int Square { get; set; }
+	public int Floors { get; set; }
+	public Building(string date, int square, int floors)
+	{
+		this.Date = date;
+		this.Square = square;
+		this.Floors = floors;
+	}
+
+	public abstract int capacity();
+}
+
+public class PublicBuilding : Building, ITicket
+{
+
+	public PublicBuilding(string date, int square, int floors) : base(date, square, floors) { }
+
+	public int CountPrice(int price, int customer_age)
+	{
+		if (customer_age <= 6 || customer_age >= 60)
+		{
+			return price / 2;
+		}
+		return price;
+	}
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 5;
+	}
+}
+
+public class ResidentialBuilding : Building, IRent
+{
+	public ResidentialBuilding(string date, int square, int floors) : base(date, square, floors) { }
+
+	public int CountRent(int price_per_meter, int square, int days)
+	{
+		return price_per_meter * square * days;
+	}
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 8;
+	}
+}
+
+public class Cinema : PublicBuilding, ITicket
+{
+	public Cinema(string date, int square, int floors) : base(date, square, floors) { }
+	public int Price { get; set; }
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 4;
+	}
+
+}
+
+public class Hotel : ResidentialBuilding, IRent
+{
+	public Hotel(string date, int square, int floors) : base(date, square, floors) { }
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 7;
+	}
+}
+
+public class City
+{
+	public List<Building> Buildings;
+	public City(List<Building> building_list)
+	{
+		this.Buildings = building_list;
+	}
+
+	public void add_building(Building building)
+	{
+		Buildings.Add(building);
+	}
+
+	public void show_building()
+	{
+		foreach (Building b in Buildings)
+		{
+			Console.WriteLine("\n" + b);
+		}
+	}
+
+	public void average_capacity()
+	{
+		int cinema_sum = 0;
+		int cinema_count = 0;
+		int hotel_sum = 0;
+		int hotel_count = 0;
+		int public_sum = 0;
+		int public_count = 0;
+		int residental_sum = 0;
+		int residental_count = 0;
+		foreach (Building b in Buildings)
+		{
+			if (b is Cinema)
+			{
+				cinema_sum += b.capacity();
+				cinema_count += 1;
+			}
+			if (b is Hotel)
+			{
+				hotel_sum += b.capacity();
+				hotel_count += 1;
+			}
+
+			if (b is PublicBuilding)
+			{
+				public_sum += b.capacity();
+				public_count += 1;
+			}
+
+			if (b is ResidentialBuilding)
+			{
+				residental_sum += b.capacity();
+				residental_count += 1;
+			}
+
+		}
+		if (cinema_count != 0)
+		{
+			Console.WriteLine("\n" + "Cередня ємність кінотеатрів: " + cinema_sum / cinema_count);
+		}
+		if (public_count != 0)
+		{
+			Console.WriteLine("\n" + "Cередня ємність громадських будівель: " + public_sum / public_count);
+		}
+		if (residental_count != 0)
+		{
+			Console.WriteLine("\n" + "Cередня ємність всіх житлових будівель: " + residental_sum / residental_count);
+		}
+		if (hotel_count != 0)
+		{
+			Console.WriteLine("\n" + "Cередня ємність всіх готелів: " + hotel_sum / hotel_count);
+		}
+	}
+}
